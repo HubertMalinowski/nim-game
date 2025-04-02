e5ac4a3d98bfcff82272f336d4f50fc6610803e3
Jasiek <jasiekk9@gmail.com>
2025-04-02 19:45:48 +0200

    Make game creation more flexible

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import random
 from nim import Nim
 
 
-def create_game():
+def create_game_from_input():
     """Creates a new game from user input"""
     stacks = list(
         map(int, input(f"Enter stack heights separated by spaces: ").split())
@@ -10,14 +10,40 @@ def create_game():
     return Nim(stacks)
 
 
+def create_random_game():
+    """Creates a random game based on parameters from the user"""
+    while True:
+        try:
+            max_stacks = int(input("Enter max number of stacks: "))
+            max_height = int(input("Enter max stack height: "))
+            break
+        except ValueError:
+            print("Enter valid numbers!")
+    stacks = [random.randint(1, max_height) for _ in range(random.randint(1, max_stacks))]
+    return Nim(stacks)
+
+
 def decide_player():
-    """Asks user to specify who should start the game"""
+    """Creates a game based on user input"""
     while True:
         choice = input("Who should start the game, (p)layer or (c)omputer? ").lower()
         if choice in ['p', 'c']:
             break
         print("Invalid input!")
     return 0 if choice == 'p' else 1
+
+
+def create_game():
+    """Queries the user about game parameters"""
+    while True:
+        choice = input("Should the game be (r)andom or from (i)nput? ")
+        if choice in ['r', 'i']:
+            break
+        print("Invalid input!")
+    if choice == 'r':
+        return create_random_game()
+    if choice == 'i':
+        return create_game_from_input()
 
 
 def human_vs_computer():
@@ -48,14 +74,12 @@ def human_vs_computer():
 
 def computer_vs_computer():
     """Simulation of computer vs computer game"""
-    stacks = [random.randint(1, 10) for _ in range(random.randint(3, 6))]
-    game = Nim(stacks)
+
+    game = create_game()
+    turn = 0  # 0 - computer 1, 1 - computer 2
 
     print("Initial game state:")
     game.print_stacks()
-
-    turn = 0  # 0 - computer 1, 1 - computer 2
-
     while not game.is_empty():
         game.computer_move()
         game.print_stacks()
